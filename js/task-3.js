8b17b5b61fdc4656352896d2ac9723a3ffe6ff02
OlegTch <Olgard984@gmail.com>
2021-10-10 17:07:45 +0300

    добавляет промисы

--- js/task-3.js
+++ js/task-3.js
@@ -7,13 +7,12 @@ const randomIntegerFromInterval = (min, max) => {
 
 const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
-
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       } else {
         reject(transaction.id);
       }
@@ -21,7 +20,7 @@ const makeTransaction = transaction => {
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
